9081f2fecc6853b85d136240484cdb40cc52d37b
barbosaf <fabio.barbosa@blip.pt>
2019-03-10 22:11:46 +0000

    Trying to fix patch version job error

--- .circleci/scripts/version.sh
+++ .circleci/scripts/version.sh
@@ -3,6 +3,9 @@ CURRENT_VERSION=$(head -n 1 VERSION)
 
 if [ -z "$CURRENT_VERSION" ]; then
     printf "1.0.0" > VERSION
+
+    NEXT_VERSION=$(head -n 1 VERSION)
+    echo "The release version will be ${NEXT_VERSION}"
     exit 0
 fi
 
@@ -12,3 +15,5 @@ NEXT_MINOR=$((${MINOR_VERSION} + 1))
 NEXT_VERSION=$(printf "${ARRAY[0]}.${ARRAY[1]}.${NEXT_MINOR}")
 
 printf ${NEXT_VERSION} > VERSION
+
+echo "The release version will be ${NEXT_VERSION}"
